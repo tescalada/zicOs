124884dd861ef72836e3629d71c7c73e26df1705
apiel <alexandre.piel@gmail.com>
2025-04-26 18:18:05 +0200

    zicOs

--- post-build.sh
+++ post-build.sh
@@ -3,25 +3,6 @@ set -e
 
 ./board/raspberrypi4-64/post-build.sh
 
-# Add dtparam=audio=on to /boot/config.txt if it doesn't exist
-# if ! grep -q "dtparam=audio=on" "${BINARIES_DIR}/rpi-firmware/config.txt"; then
-#     echo "\n# Enable audio" >> "${BINARIES_DIR}/rpi-firmware/config.txt"
-#     echo "dtparam=audio=on" >> "${BINARIES_DIR}/rpi-firmware/config.txt"
-# fi
-
-# Add dtparam=spi=on to /boot/config.txt if it doesn't exist
-if ! grep -q "dtparam=spi=on" "${BINARIES_DIR}/rpi-firmware/config.txt"; then
-    echo "\n# Enable SPI" >> "${BINARIES_DIR}/rpi-firmware/config.txt"
-    echo "dtparam=spi=on" >> "${BINARIES_DIR}/rpi-firmware/config.txt"
-    echo "dtoverlay=spi0-0" >> "${BINARIES_DIR}/rpi-firmware/config.txt"
-fi
-
-# Add dtparam=i2c=on to /boot/config.txt if it doesn't exist
-if ! grep -q "dtparam=i2c=on" "${BINARIES_DIR}/rpi-firmware/config.txt"; then
-    echo "\n# Enable I2C" >> "${BINARIES_DIR}/rpi-firmware/config.txt"
-    echo "dtparam=i2c=on" >> "${BINARIES_DIR}/rpi-firmware/config.txt"
-fi
-
 echo "/dev/mmcblk0p1 /boot vfat defaults 0 2" >> ${TARGET_DIR}/etc/fstab
 
 cd output/target
